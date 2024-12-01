1c84ea02aea5a40901a9636e3e0380d5286c95fb
siddharthaasal <siddharthaasal@gmail.com>
2024-12-02 00:35:39 +0530

    Day 01

--- Day01.java
+++ Day01.java
@@ -18,7 +18,7 @@ public class Day01 {
         }
     }
 
-    private static int solvePart1(String input){
+    private static int solvePart1(String input) {
 
         String[] lines = input.split("\\R");
         int[] l1 = new int[lines.length];
@@ -26,7 +26,7 @@ public class Day01 {
 
         for (int i = 0; i < lines.length; i++) {
             String[] parts = lines[i].split("\\s+"); // Split on one or more spaces
-            l1[i] = Integer.parseInt(parts[0]); 
+            l1[i] = Integer.parseInt(parts[0]);
             l2[i] = Integer.parseInt(parts[1]);
         }
 
@@ -35,24 +35,24 @@ public class Day01 {
 
         int ans = 0;
 
-        for(int i=0; i<lines.length; i++){
+        for (int i = 0; i < lines.length; i++) {
             ans += Math.abs(l1[i] - l2[i]);
         }
 
         return ans;
     }
 
-    private static int solvePart2(String input){
-       String[] lines = input.split("\\R");
+    private static int solvePart2(String input) {
+        String[] lines = input.split("\\R");
         int[] l1 = new int[lines.length];
         int[] l2 = new int[lines.length];
 
         for (int i = 0; i < lines.length; i++) {
             String[] parts = lines[i].split("\\s+"); // Split on one or more spaces
-            l1[i] = Integer.parseInt(parts[0]); 
+            l1[i] = Integer.parseInt(parts[0]);
             l2[i] = Integer.parseInt(parts[1]);
         }
-        
+
         HashMap<Integer, Integer> rightListFreq = new HashMap<>();
 
         for (int i = 0; i < l2.length; i++) {
@@ -62,10 +62,10 @@ public class Day01 {
 
         int ans = 0;
 
-        for(int i=0; i<l1.length; i++){
+        for (int i = 0; i < l1.length; i++) {
             int n = l1[i];
-            if(rightListFreq.containsKey(n)){
-                ans += n*rightListFreq.get(n);
+            if (rightListFreq.containsKey(n)) {
+                ans += n * rightListFreq.get(n);
             }
         }
 
